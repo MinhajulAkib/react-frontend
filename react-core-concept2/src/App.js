2bfdcef01487d0d9ac9cfa7e78178b998b98120e
MinhajulAkib <00akibswe@gmail.com>
2021-09-18 22:39:09 +0600

    dynamic data load

--- react-core-concept2/src/App.js
+++ react-core-concept2/src/App.js
@@ -1,14 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
     </div>
-  );
+  ); 
 }
+function ExternalUsers(){
+  const [users, setUsers] = useState([]); 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+  return (
+    <div>
+      <h1>ExternalUsers</h1>
+      {
+        users.map(user => <User name= {user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+function User(props){
+  const UserStyle = {
+    border:'3px solid blue',
+    borderRadius:'10px',
+    margin:'10px',
+    padding:'10px'
+  }
+  return(
+    <div className="User" style={UserStyle}>
+      <h2>name:{props.name}</h2>
+      <p>Email:{props.email}</p>
+    </div>
+  )
+}
+
 function Counter(){
   const [count, setCount] = useState(0);
   const handleIncrease = () =>  setCount(count +1);
