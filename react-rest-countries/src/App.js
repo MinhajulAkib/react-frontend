5662647575e2ccc53dc13ecaca98dd7c99597aa8
MinhajulAkib <00akibswe@gmail.com>
2021-09-20 10:11:53 +0600

    display countries

--- react-rest-countries/src/App.js
+++ react-rest-countries/src/App.js
@@ -13,13 +13,25 @@ function Countries(){
   useEffect(() => {
      fetch('https://restcountries.eu/rest/v2/all')
      .then(res => res.json())
-     .then(data => setCountries(data))
+     .then(data => setCountries(data));
   }, [])
   return (
     <div>
       <h2>Travelling around the World</h2>
       <h4>Contries availabe: {countries.length}</h4>
+      {
+        countries.map(country => <Country name ={country.name} capital={country.capital}></Country>)
+      }
     </div>
   );
+}
+
+function Country(props){
+  return (
+    <div>
+        <h2>Name: {props.name}</h2>
+        <h4>Capital: {props.capital}</h4>
+    </div>
+  )
 }
 export default App;
