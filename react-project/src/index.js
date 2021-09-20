76c2d5d0bda33f798693371d9534072dc7073305
MinhajulAkib <00akibswe@gmail.com>
2021-09-20 21:33:42 +0600

    react-project

--- react-project/src/index.js
+++ react-project/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./index.css";
 
 const todoTitle ="Call Family";
 const todoDesc = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea laudantium est alias, iure consequatur eum nobis ipsam nihil nam atque?";
@@ -8,9 +9,16 @@ const dateName = date.getDate();
 const monthName = date.getMonth();
 const yearName = date.getFullYear();
 
+// const headingStyle ={
+//   backgroundColor:'purple',
+//   color:'white',
+//   textAlign:'center',
+//   padding:'15px'
+// }
+
 ReactDOM.render(
   <div>
-    <h1>Todo App</h1>
+    <h1 className="headingStyle">Todo App</h1>
     <h3>{todoTitle}</h3>
     <p>{todoDesc}</p>
     <p>{dateName + " /" + monthName + " /" + yearName}</p>
